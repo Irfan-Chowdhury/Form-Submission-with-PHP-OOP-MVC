8d3e6054d91c3f415869edc7861349d329b7460c
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2024-08-27 19:33:03 +0600

    MVC primary layout setup

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$router = require '../src/Routes/index.php';
+
+
+
+
+// https://maheshsamudra.medium.com/creating-a-simple-php-mvc-framework-from-scratch-7158f12340a0
